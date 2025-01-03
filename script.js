4ffe3a7abe68e05c449a5f13d2980e7a7930e4a2
twinkleughade <twinklethakre1998@gmail.com>
2025-01-03 23:17:32 +0530

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// -----------------------------------          navbar           ----------------------------------//
 const bar=document.getElementById('bar');
 const close=document.getElementById('close');
 const nav=document.getElementById('navbar');
@@ -12,3 +13,23 @@ if(close){
         nav.classList.remove('active')
     })
 }
+
+//----------------------------------     single product         --------------------------------//
+var mainimg = document.getElementById("mainimg");
+var smallimg = document.getElementsByClassName("small-img");
+
+smallimg[0].onclick =function(){
+    mainimg.src =smallimg[0].src;
+}
+smallimg[1].onclick =function(){
+    mainimg.src =smallimg[1].src;
+}
+smallimg[2].onclick =function(){
+    mainimg.src =smallimg[2].src;
+}
+smallimg[3].onclick =function(){
+    mainimg.src =smallimg[3].src;
+}
+
+
+
